f7859f02a0f7746a579caa6041b31dfbbdcc9bbb
T.Petrukhina <libelulalala@gmail.com>
2016-02-21 17:18:34 +0300

    добавлено модальное окно

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
+// Открытие-закрытие модального окна
+// Переменные
+var link = document.querySelector(".btn--popup-show");
+var popup = document.querySelector(".popup");
+var close = document.querySelector(".popup__btn-close");
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name='username']");
+var email = popup.querySelector("[name='email']");
+var storage = localStorage.getItem("username");
 
+// Открываем модальное окно
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("popup-show");
+    if (storage) {
+      username.value = storage;
+      email.focus();
+    } else {
+      username.focus();
+    }
+});
+
+// Закрываем модальное окно
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("popup-show");
+});
+window.addEventListener("keydown", function(event) {
+if (event.keyCode == 27) {
+          if (popup.classList.contains("popup-show")) {
+            popup.classList.remove("popup-show");
+          }
+        }
+});
+
+
+// Интерактивная карта
 ymaps.ready(init);
 var myMap;
 function init() {
